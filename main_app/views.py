5ef0b5c6ea7e45f14fd0ebaddc0bbd1d36f5d904
Alan McGurney <mcgurneyalan@gmail.com>
2022-02-08 17:36:08 -0500

    third commit

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import View # <- View class to handle requests
+from django.http import HttpResponse # <- a class to handle sending a type of response
+
 
 # Create your views here.
+
+# Here we will be creating a class called Home and extending it from the View class
+class Home(View):
+
+    # Here we are adding a method that will be ran when we are dealing with a GET request
+    def get(self, request):
+        # Here we are returning a generic response
+        # This is similar to response.send() in express
+        return HttpResponse("Lou Reed Home")
+
+class About(View):
+
+    def get(self, request):
+        return HttpResponse("Spotify About")
