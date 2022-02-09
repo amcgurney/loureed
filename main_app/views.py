bb51db59432b786a5bf6c16a33170e089b6a65ba
Alan McGurney <mcgurneyalan@gmail.com>
2022-02-08 22:15:31 -0500

    thirteen commits added songs class

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View # <- View class to handle requests
 from django.http import HttpResponse # <- a class to handle sending a type of response
 from django.views.generic.base import TemplateView
-
+from .models import Songs
 # Create your views here.
 
 # Here we will be creating a class called Home and extending it from the View class
@@ -24,10 +24,10 @@ class About(View):
         return HttpResponse("Lou Reed About")
 
 songs = [
-  Song("Sweet Jane", "https://hips.hearstapps.com/esq.h-cdn.co/assets/cm/15/06/54d1ede2a7224_-_esq-102813-lou-reed-2.jpg",
+  Songs("Sweet Jane", "https://hips.hearstapps.com/esq.h-cdn.co/assets/cm/15/06/54d1ede2a7224_-_esq-102813-lou-reed-2.jpg",
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit."),
-  Song("Who Loves the Sun", "https://variety.com/wp-content/uploads/2019/10/2019_1599_004-e1572475947414.jpg?w=681&h=383&crop=1", "Sed varius in sem convallis rutrum."),
-  Song("Oh! Sweet Nuthin'", "https://www.telegraph.co.uk/content/dam/books/2017/12/28/TELEMMGLPICT000149622020_trans_NvBQzQNjv4BqplGOf-dgG3z4gg9owgQTXDVXE4-NcPVfcZy5a1cUJ04.jpeg?imwidth=680", "Curabitur ut ultrices neque."),
+  Songs("Who Loves the Sun", "https://variety.com/wp-content/uploads/2019/10/2019_1599_004-e1572475947414.jpg?w=681&h=383&crop=1", "Sed varius in sem convallis rutrum."),
+  Songs("Oh! Sweet Nuthin'", "https://www.telegraph.co.uk/content/dam/books/2017/12/28/TELEMMGLPICT000149622020_trans_NvBQzQNjv4BqplGOf-dgG3z4gg9owgQTXDVXE4-NcPVfcZy5a1cUJ04.jpeg?imwidth=680", "Curabitur ut ultrices neque."),
 ]
 
 class SongList(TemplateView):
