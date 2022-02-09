637507678a6239ecfdd3b8eb748149d00ede65ac
Alan McGurney <mcgurneyalan@gmail.com>
2022-02-09 11:09:24 -0500

    fifteen commits song create added

---
+++ db/create_songs.sql
@@ -0,0 +1,11 @@
+INSERT INTO main_app_song(name, img, bio)
+  VALUES
+    ('Andys Chest','https://i1.sndcdn.com/artworks-hFzyGeLAFVXByFTv-xuX8eQ-t500x500.jpg','Duis pharetra nunc sit amet sem fermentum, at porttitor est sagittis.');
+
+INSERT INTO main_app_songs(name, img, bio)
+  VALUES
+    ('Ride into the Sun','https://shapersofthe80s.files.wordpress.com/2013/10/reed69shades.jpg','Sed in condimentum nisl. Nullam faucibus faucibus venenatis.');
+
+INSERT INTO main_app_songs(name, img, bio)
+  VALUES
+    ('The Gift','https://aquariumdrunkard.com/_newness/wp-content/uploads/2008/08/velvet-underground.jpg','Pellentesque sed dictum leo, nec consequat enim.');
